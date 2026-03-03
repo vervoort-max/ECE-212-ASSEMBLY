ac9142bad97108d0c072b1b4455485212b921b0f
Jenna Vervoort <vervoort@ualberta.ca>
2026-03-03 10:41:25 -0700

    working on lab2

--- lab2/partA/1.asm
+++ lab2/partA/1.asm
@@ -2,5 +2,9 @@
 .global start
 
 start:
-
+LDR R0, =0x20001000 @size of the array
+LDR R1, [R0]
+LDR R2, [R0, #4] @Address of the first array
+LDR R3, [R0, #8] @Address of the second array
+LDR R4, [R0, #12] @Address of where to store the sum with Register Indirect w Offset
 
