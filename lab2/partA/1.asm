834228b06d035dde5f0387d2e733f94632547c99
Jenna Vervoort <vervoort@ualberta.ca>
2026-03-03 10:48:23 -0700

    maybe finished lab2 A1

--- lab2/partA/1.asm
+++ lab2/partA/1.asm
@@ -2,9 +2,27 @@
 .global start
 
 start:
-LDR R0, =0x20001000 @size of the array
-LDR R1, [R0]
+LDR R0, =0x20001000
+LDR R1, [R0] @Size of the array
 LDR R2, [R0, #4] @Address of the first array
 LDR R3, [R0, #8] @Address of the second array
 LDR R4, [R0, #12] @Address of where to store the sum with Register Indirect w Offset
 
+MOV R5, #0 @i < 3 - Loop counter
+
+loop:
+CMP R5, #3 @Checking if counter is 3
+BEQ done @leaving loop
+
+LSL R6, R5, #2 @Essentially - offset = i(R5) * 4(LSL R5, #2)
+LDR R7, [R2, R6] @A[i] - also (R2 + R6)
+LDR R8, [R3, R6] @B[i] - also (R3 + R6)
+
+ADD R9, R7, R8 @A[i] + B[i]
+STR R9, [R4, R6]
+
+ADD R5, R5, #1 @Increment counter
+
+B loop
+
+done:
